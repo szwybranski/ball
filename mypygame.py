569870916d42e9df04f0bd73a3baf1efec4db979
Richard Cook <rcook@rcook.org>
2017-03-26 15:47:23 -0700

    Enable resizing

--- mypygame.py
+++ mypygame.py
@@ -1,33 +1,53 @@
-# Taken from https://www.pygame.org/docs/tut/PygameIntro.html
+# Resources:
+# https://www.pygame.org/docs/tut/PygameIntro.html
+# http://stackoverflow.com/questions/11603222/allowing-resizing-window-pygame
 
 from __future__ import print_function
 import sys, pygame
-#from pygame.locals import *
+from pygame.locals import *
 from time import sleep
 
 pygame.init()
 
-size = width, height = 320, 240
 speed = [2, 2]
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
-#screen = pygame.display.set_mode(size, RESIZABLE)
+screen = pygame.display.set_mode((320, 240), RESIZABLE)
+pygame.display.set_caption("Resizing test")
 
 ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+ball_rect = ball.get_rect()
 
-while 1:
+def draw_screen():
+    screen.fill((0, 0, 255))
+    pygame.draw.rect(
+        screen,
+        (200, 0, 0),
+        (screen.get_width() / 3, screen.get_height() / 3, screen.get_width() / 3, screen.get_height()/ 3))
+
+    pygame.display.update()
+
+draw_screen()
+
+while True:
+    print("Loop {}".format(screen.get_width()))
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+        elif event.type == VIDEORESIZE:
+            screen = pygame.display.set_mode(
+                (event.w, event.h),
+                RESIZABLE)
+            draw_screen()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    ball_rect = ball_rect.move(speed)
+    if ball_rect.left < 0 or ball_rect.right > screen.get_width():
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ball_rect.top < 0 or ball_rect.bottom > screen.get_height():
         speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(ball, ball_rect)
     pygame.display.flip()
-    sleep(0.01)
