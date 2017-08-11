d377c955da06b62e0c698c6e951299050f0fa7cc
Richard Cook <rcook@rcook.org>
2017-08-10 19:30:20 -0700

    Fix bootstrap scripts

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -5,9 +5,22 @@ set python_path=%~dp0Scripts\python.exe
 set pip_path=%~dp0Scripts\pip.exe
 
 if not exist "%python_path%" (
-    cd "%~dp0"
-    virtualenv .
+    cd /d "%~dp0"
+    python -m virtualenv .
+    if errorlevel 1 (
+        echo virtualenv failed
+        exit /b 1
+    )
 )
 
 "%pip_path%" install -q -r "%~dp0requirements.txt"
+if errorlevel 1 (
+    echo pip failed
+    exit /b 1
+)
+
 "%python_path%" %*
+if errorlevel 1 (
+    echo script failed
+    exit /b 1
+)
--- mypygame.cmd
+++ mypygame.cmd
@@ -1,2 +1,6 @@
 @echo off
 call "%~dp0bootstrap.cmd" "%~dp0mypygame.py" %*
+if errorlevel 1 (
+    echo script failed
+    exit /b 1
+)
